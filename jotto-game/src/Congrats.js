07838c13fd4b5e4aeff71e254ca6190335f32cf7
ercardona <ercardona@edrans.com>
2021-07-18 17:25:58 +0200

    add proptypes for Congrats component

--- jotto-game/src/Congrats.js
+++ jotto-game/src/Congrats.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /**
  * @function
@@ -17,6 +18,10 @@ const Congrats = (props) => {
   } else {
     return <div data-test="component-congrats"></div>;
   }
+};
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
 };
 
 export default Congrats;
--- jotto-game/test/testUtils.js
+++ jotto-game/test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from "check-prop-types";
+
 /**
  * Return node(s) with the given data-test attribute.
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
@@ -5,5 +7,15 @@
  * @returns {ShallowWrapper}
  */
 export const findbyTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test="${val}"]`);
-}
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+export const checkProps = (component, conformimngProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformimngProps,
+    "prop",
+    component.name
+  );
+  expect(propError).toBeUndefined();
+};
